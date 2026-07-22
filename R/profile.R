3f0a0fcaaf6f0905bfb925e0d7205f4f11b3628b
eblondel <emmanuel.blondel1@gmail.com>
2026-07-22 23:46:50 +0200

    make silent sf_use_s2

--- R/profile.R
+++ R/profile.R
@@ -1,5 +1,6 @@
 .onLoad <- function (libname, pkgname) {
  
-  sf::sf_use_s2(FALSE) 
+  #make sure sf relies on... "sf" (aka ISO/OGC Simple Features)! and not s2
+  options(sf_use_s2 = FALSE)
   
 }
